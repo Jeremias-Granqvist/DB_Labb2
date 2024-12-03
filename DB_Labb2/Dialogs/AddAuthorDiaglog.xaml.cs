ded8a3d0716c587e427ebcc83868553ef2d616ed
Jeremias Granqvist <Jeremias.granqvist@gmail.com>
2024-12-03 17:01:09 +0100

    Add project files.

---
+++ DB_Labb2/Dialogs/AddAuthorDiaglog.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DB_Labb2.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for AddAuthorDiaglog.xaml
+    /// </summary>
+    public partial class AddAuthorDiaglog : Window
+    {
+        public AddAuthorDiaglog()
+        {
+            InitializeComponent();
+        }
+
+        private void FirstNameTB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ DB_Labb2/Dialogs/AlterBook.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DB_Labb2.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for AlterBook.xaml
+    /// </summary>
+    public partial class AlterBook : Window
+    {
+        public AlterBook()
+        {
+            InitializeComponent();
+        }
+    }
+}
